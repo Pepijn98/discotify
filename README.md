01f7e1c51f6513453dbec30f8bc6ca40ca9e56bc
KurozeroPB <pepijn.vanden.broek@gmail.com>
2017-12-02 00:27:44 +0100

    0.2.0 Added cli compatibility

--- README.md
+++ README.md
@@ -1,29 +1,27 @@
-# discotify `0.1.4`
+# discotify `0.2.0`
 Spotify Rich Presence for Discord in TypeScript<br/>
 *shows your currently playing song as a fancy rich presence in Discord*
+
+Install globally with:<br/>
+`npm install -g discotify`
 
 ## Requirements
   - NodeJS: best to get v8 or above. Get the one for your OS [here](https://nodejs.org/en/download/package-manager)
   - npm: Should automatically be installed with nodejs
-  - git: Get it from [here](https://git-scm.com/downloads)
   - Discord-ptb or canary:
     - ptb you can get from [here](https://discordapp.com/download) by clicking on "Get the Public Test Build"
     - Canary links: [win](https://discordapp.com/api/download/canary?platform=win), [linux](https://discordapp.com/api/download/canary?platform=linux), [osx](https://discordapp.com/api/download/canary?platform=osx)
   - Spotify: [win](https://www.spotify.com/nl/download/windows/), [linux](https://www.spotify.com/nl/download/linux/), [osx](https://www.spotify.com/nl/download/mac/)
+  - You might additionally have to install ts-node and typescript globally
+    - ts-node: `npm install -g ts-node` 
+    - typescript: `npm install -g typescript`
 
-## Setup
-  - `git clone https://github.com/KurozeroPB/discotify.git`
-  - `cd discotify` or w/e you named the folder
-  - `npm i`
-  - `npm i -g typescript`
-  - `npm i -g ts-node`
-  - Open Discord and Spotify
-  - Start a song
-  - Launch the app with `npm start` or `ts-node index.ts`
-
-## Updating
-If you still have the .git folder just doing `git pull` should work<br/>
-else you should delete the existing version and follow the first 3 steps from the **Setup** and start it like normally.
+## Usage
+To start use:<br/>
+`discotify start`<br/>
+<br/>
+To stop:<br/>
+`ctrl + c` does the job
 
 ## Tested on
   - Linux Ubuntu 16.04 ✅
--- index.ts
+++ index.ts
@@ -1,111 +1,124 @@
+#!/usr/bin/env ts-node
+
 import * as DiscordRPC from 'discord-rpc';
 import * as logger from 'winston';
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios from 'axios';
 import { platform } from 'os';
 
-let nodeSpotifyWebhelper;
-if (platform().toString() === "win32") {
-	nodeSpotifyWebhelper = require('./spotify-webhelper/windows');
-} else {
-	nodeSpotifyWebhelper = require('./spotify-webhelper/linux');
-}
+const [, , ...args] = process.argv;
 
-const spotify = new nodeSpotifyWebhelper.SpotifyWebHelper();
-const rpc = new DiscordRPC.Client({ transport: 'ipc' });
-const clientID = '383639700994523137';
-let compareURI: string;
-let compare: boolean;
-
-logger.configure({
-	transports: [
-		new logger.transports.Console({
-			colorize: true
-		})
-	]
-});
-
-function updateRichPresence() {
-	if (!rpc) return;
-
-	spotify.getStatus((err: Error, res: any) => {
-		if (err) return logger.error(err.stack ? err.stack : err.toString());
-		if (!res.track || !res.track.track_resource) return logger.warn(`(${new Date().toLocaleTimeString()}) No track data, make sure Spotify is opened and a song is selected!`);
-
-		if (res.playing === true) {
-			compare = false;
-			if (res.track.track_resource.uri === compareURI) return;
-
-			let start = parseInt(new Date().getTime().toString().substr(0, 10));
-			let end = start + (res.track.length - res.playing_position);
-			compareURI = res.track.track_resource.uri;
-
-			rpc.setActivity({
-				details: `🎵 Song - ${res.track.track_resource.name}`,
-				state: `👤 Artist - ${res.track.artist_resource.name}`,
-				startTimestamp: start,
-				endTimestamp: end,
-				largeImageKey: 'spotifylarge',
-				largeImageText: `💿 Album - ${res.track.album_resource.name}`,
-				smallImageKey: 'playing',
-				smallImageText: 'Playing',
-				instance: false,
-			});
-
-			logger.info(`(${new Date().toLocaleTimeString()}) Updated Song - ${res.track.track_resource.name} by ${res.track.artist_resource.name}`);
-		} else {
-			if (compare === true) return;
-			compare = true;
-			compareURI = '';
-
-			rpc.setActivity({
-				details: `Paused`,
-				startTimestamp: 0,
-				endTimestamp: 0,
-				largeImageKey: 'spotifylarge',
-				largeImageText: '---',
-				smallImageKey: 'paused',
-				smallImageText: 'Paused',
-				instance: false
-			});
-
-			logger.info(`(${new Date().toLocaleTimeString()}) Spotify is paused`);
-		}
-	});
-};
-
-function checkVersion(): Promise<string> {
-  return new Promise((resolve, reject) => {
-		let sVersion: string = require('./package.json').version;
-		let version = ~~(require('./package.json').version.split('.').join(''));
-  	axios.get('https://raw.githubusercontent.com/KurozeroPB/discotify/master/package.json')
-			.then((res: AxiosResponse) => {
-				if (res.status !== 200) {
-					return reject(new Error(`Failed to check for updates: ${res.data}`));
-				} else {
-					let latest = ~~(res.data.version.split('.').join(''));
-					if (latest > version) return reject(new Error(`A new version of Discotify is avalible\nPlease get the latest version from: https://github.com/KurozeroPB/discotify`));
-					return resolve(`Discotify is up-to-date using v${sVersion}`);
-				}
-			}).catch((err: AxiosError) => {
-				return reject(new Error(err.stack ? err.stack : err.message ? err.message : err.toString()));
-			});
-	});
-};
-
-rpc.on('ready', async () => {
-	try {
-		const resp = await checkVersion();
-		logger.info(resp);
-	} catch (e) { 
-		return logger.error(e.message ? e.message : e);
+if (args[0].toLocaleLowerCase() === "start") {
+	let nodeSpotifyWebhelper;
+	if (platform().toString() === "win32") {
+		nodeSpotifyWebhelper = require('./spotify-webhelper/windows');
+	} else {
+		nodeSpotifyWebhelper = require('./spotify-webhelper/linux');
 	}
 
-	logger.info(`Connected with ID: ${clientID}`);
-	updateRichPresence();
-	setInterval(() => {
-		updateRichPresence();
-	}, 1500);
-});
+	const spotify = new nodeSpotifyWebhelper.SpotifyWebHelper();
+	const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+	const clientID = '383639700994523137';
+	let compareURI: string;
+	let compare: boolean;
 
-rpc.login(clientID)
-	.catch((err: Error) => logger.error(err.stack ? err.stack : err.toString()));
+	logger.configure({
+		transports: [
+			new logger.transports.Console({
+				colorize: true
+			})
+		]
+	});
+
+	function updateRichPresence() {
+		if (!rpc) return;
+
+		spotify.getStatus((err: Error, res: any) => {
+			if (err) return logger.error(err.stack ? err.stack : err.toString());
+			if (!res.track || !res.track.track_resource) return logger.warn(`(${new Date().toLocaleTimeString()}) No track data, make sure Spotify is opened and a song is selected!`);
+
+			if (res.playing === true) {
+				compare = false;
+				if (res.track.track_resource.uri === compareURI) return;
+
+				let start = parseInt(new Date().getTime().toString().substr(0, 10));
+				let end = start + (res.track.length - res.playing_position);
+				compareURI = res.track.track_resource.uri;
+
+				rpc.setActivity({
+					details: `🎵 Song - ${res.track.track_resource.name}`,
+					state: `👤 Artist - ${res.track.artist_resource.name}`,
+					startTimestamp: start,
+					endTimestamp: end,
+					largeImageKey: 'spotifylarge',
+					largeImageText: `💿 Album - ${res.track.album_resource.name}`,
+					smallImageKey: 'playing',
+					smallImageText: 'Playing',
+					instance: false,
+				});
+
+				logger.info(`(${new Date().toLocaleTimeString()}) Updated Song - ${res.track.track_resource.name} by ${res.track.artist_resource.name}`);
+			} else {
+				if (compare === true) return;
+				compare = true;
+				compareURI = '';
+
+				rpc.setActivity({
+					details: `Paused`,
+					startTimestamp: 0,
+					endTimestamp: 0,
+					largeImageKey: 'spotifylarge',
+					largeImageText: '---',
+					smallImageKey: 'paused',
+					smallImageText: 'Paused',
+					instance: false
+				});
+
+				logger.info(`(${new Date().toLocaleTimeString()}) Spotify is paused`);
+			}
+		});
+	};
+
+	function checkVersion(): Promise<string> {
+		return new Promise((resolve, reject) => {
+			let sVersion: string = require('./package.json').version;
+			let version = ~~(require('./package.json').version.split('.').join(''));
+			axios.get('https://raw.githubusercontent.com/KurozeroPB/discotify/master/package.json')
+				.then((res: any) => {
+					if (res.status !== 200) {
+						return reject(new Error(`Failed to check for updates: ${res.data}`));
+					} else {
+						let latest = ~~(res.data.version.split('.').join(''));
+						if (latest > version) return reject(new Error(`A new version of Discotify is avalible\nPlease get the latest version from: https://github.com/KurozeroPB/discotify`));
+						return resolve(`Discotify is up-to-date using v${sVersion}`);
+					}
+				}).catch((err: any) => {
+					return reject(new Error(err.stack ? err.stack : err.message ? err.message : err.toString()));
+				});
+		});
+	};
+
+	rpc.on('ready', async () => {
+		try {
+			const resp = await checkVersion();
+			logger.info(resp);
+		} catch (e) {
+			return logger.error(e.message ? e.message : e);
+		}
+
+		logger.info(`Connected with ID: ${clientID}`);
+		updateRichPresence();
+		setInterval(() => {
+			updateRichPresence();
+		}, 1500);
+	});
+
+	rpc.login(clientID)
+		.catch((err: Error) => logger.error(err.stack ? err.stack : err.toString()));
+} else {
+	console.log(`
+ _______________________________________
+|                                       |
+| discotify start - to start the script |
+|_______________________________________|
+	`);
+}
